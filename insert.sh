5f87dc657d19b22ed38ba15bb9ddaf7a98d27c63
mohammed Adel <mohammedadell301@gmail.com>
2022-12-26 18:09:09 +0200

    commit

--- insert.sh
+++ insert.sh
@@ -4,31 +4,44 @@ export LC_COLLATE=C
 declare -a feilds
 declare -a ar_type
 declare -a IDArray
-declare -a preInsertedData
 declare -a insertedData
-
+intRegex='^[0-9]+$'
+stringRegex='^[a-zA-z]+$'
 read -p "Enter table name " name
 arr_type=($(sed -n '2p' $name))
 
 IDArray=($(sed '1,2d' $name | cut -d' ' -f1))
 feilds=($(sed -n '1p' $name))
+# test
 echo ${feilds[@]}
 echo ${arr_type[@]}
 echo ${IDArray[@]}
+# length of feilds array
 len=${#feilds[@]}
+# lenOfIdArray=${#IDArray[@]}
 echo $len
+echo ${arr_type[1]}+ '0'
 for ((i=0; i<$len; i++))
 do
     read -p "enter the ${feilds[$i]} : " insertedData[$i]
     if [[ ${arr_type[$i]} == "string" ]];then
-        if [[ ${insertedData[$i]} == ^[a-zA-z]+$ ]];then
+        if [[ ${insertedData[$i]} =~ $stringRegex ]];then
             continue;
         else
             echo "wrong value for type string" 
             break;
         fi
     else 
-        if [[ ${insertedData[$i]} == ^[0-9]+$ ]];then
+        if [[ ${insertedData[$i]} =~ $intRegex ]];then
+            for ((j=0; j<${#IDArray[@]}; j++))
+            do
+                echo ${insertedData[$i]}
+                echo ${IDArray[$j]}
+                if [[ ${insertedData[$i]} == ${IDArray[$j]} ]];then
+                echo "ID must be unique";
+                read -p "enter the ${feilds[$i]} : " insertedData[$i];
+                fi
+            done
             continue;
         else
             echo "wrong value for type int" 
@@ -36,3 +49,5 @@ do
         fi
     fi
 done
+echo "data Inserted"
+echo ${insertedData[@]} >> $name
