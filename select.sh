a757e3f7e63631125e93f0b9051fe90ba5d98d98
Ibrahim Salah <ibrahimsalah824@gmail.com>
2022-12-27 18:57:19 +0200

    select

--- select.sh
+++ select.sh
@@ -11,10 +11,51 @@ do
         fi
         ;; 
         SelectByCol )
-        read -p "Please enter table name: " name
-        read -p "select * from table where =  " where 
-        read -p " = " value
-        sed -n [[ ^$where ]] 
+        read -p "Please enter table name : " name
+            if [[ -f $name ]];then
+                read -p "select * from $name where column : " column
+                declare -i flag=0
+                declare -a feilds
+                feilds=($(sed -n '1p' $name))
+                len=${#feilds[@]}
+                echo ${feilds[@]}
+                echo $len
+                for ((i=0; i<$len; i++))
+                do
+                    if [[ $column == ${feilds[$i]} ]];then
+                        read -p "value = " value
+                        echo $name
+                        awk -F" " -v userData="$value" '
+                        BEGIN{
+                            f=0
+                        }
+                        {
+                            i=1
+                            while(i <= NF){
+                                if($i == userData)
+                                {
+                                    print $0
+                                    f=1
+                                }
+                                i++
+                            }
+                        }
+                        END{
+                            if(f == 0){
+                                print "no values found"
+                            }
+                        }
+                        ' $name
+                        flag=1
+                        break;
+                    fi
+                done
+                if [[ $flag == 0 ]];then
+                    echo "there's no column found"
+                fi
+            else
+            echo "table not found";
+            fi
         ;; 
         SelectByRows ) 
         ;;
