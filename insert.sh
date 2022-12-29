d3cc23632bae4fe379f1ab12d52d8cfb27f745b9
mohammed Adel <mohammedadell301@gmail.com>
2022-12-29 18:14:31 +0200

    merge

--- insert.sh
+++ insert.sh
@@ -8,7 +8,7 @@ declare -a insertedData
 intRegex='^[0-9]+$'
 stringRegex='^[a-zA-z]+$'
 read -p "Enter table name " name
-if [[ -f $name ]];then
+if [[ -f $name ]]; then
     arr_type=($(sed -n '2p' $name))
 
     IDArray=($(sed '1,2d' $name | cut -d' ' -f1))
@@ -22,36 +22,34 @@ if [[ -f $name ]];then
     # lenOfIdArray=${#IDArray[@]}
     echo $len
     echo ${arr_type[1]}+ '0'
-    for ((i=0; i<$len; i++))
-    do
+    for ((i = 0; i < $len; i++)); do
         read -p "enter the ${feilds[$i]} : " insertedData[$i]
-        if [[ ${arr_type[$i]} == "string" ]];then
-            if [[ ${insertedData[$i]} =~ $stringRegex ]];then
-                continue;
+        if [[ ${arr_type[$i]} == "string" ]]; then
+            if [[ ${insertedData[$i]} =~ $stringRegex ]]; then
+                continue
             else
-                echo "wrong value for type string" 
-                break;
+                echo "wrong value for type string"
+                break
             fi
-        else 
-            if [[ ${insertedData[$i]} =~ $intRegex ]];then
-                for ((j=0; j<${#IDArray[@]}; j++))
-                do
+        else
+            if [[ ${insertedData[$i]} =~ $intRegex ]]; then
+                for ((j = 0; j < ${#IDArray[@]}; j++)); do
                     echo ${insertedData[$i]}
                     echo ${IDArray[$j]}
-                    if [[ ${insertedData[$i]} == ${IDArray[$j]} ]];then
-                    echo "ID must be unique";
-                    read -p "enter the ${feilds[$i]} : " insertedData[$i];
+                    if [[ ${insertedData[$i]} == ${IDArray[$j]} ]]; then
+                        echo "ID must be unique"
+                        read -p "enter the ${feilds[$i]} : " insertedData[$i]
                     fi
                 done
-                continue;
+                continue
             else
-                echo "wrong value for type int" 
-                break;
+                echo "wrong value for type int"
+                break
             fi
         fi
     done
     echo "data Inserted"
-    echo ${insertedData[@]} >> $name
-    else
+    echo ${insertedData[@]} >>$name
+else
     echo "table not found"
 fi
--- update.sh
+++ update.sh
@@ -11,52 +11,48 @@ feilds=($(sed -n '1p' $name))
 PKArray=($(sed '1,2d' $name | cut -d' ' -f1))
 len=${#feilds[@]}
 
-if [[ -f $name ]];then
+if [[ -f $name ]]; then
     read -p "set : " column
     flag=0
-    for (( i=0 ;i<$len; i++ ))
-        do 
-            if [[ $column == ${feilds[$i]} ]];then
-                read -p "$column = " value
-                ((i++));
-                # echo "iteration : " $i;
-                valuesOfColumn=($(sed '1,2d' $name | cut -d' ' -f$i))
-                # echo ${valuesOfColumn[@]}
-                    for ((j=0 ;j<${#valuesOfColumn[@]}; j++))
-                    do
-                        if [[ $value == ${valuesOfColumn[$j]} ]];then
-                            let c=$j+3
-                            sed -n "$c"p $name
-                            read -p "Do you want to update this row ? Y/N" answer
-                            if [[ $answer == 'y' || $answer == 'Y' ]];then
-                                read -p "enter new value : " newValue
-                                f=0
-                                for (( k=0;k<${#PKArray[@]};k++ ))
-                                do 
-                                    if [[ $newValue == ${PKArray[$k]} ]];then
-                                        f=1;
-                                        echo "Primary key must be unique !!!"
-                                        break;
-                                    fi
-                                done
-                                if [[ $f == 0 ]];then
-                                sed -i "${c}s/$value/$newValue/" $name
-                                fi
-                            elif [[ $answer == 'n' || $answer == 'N' ]];then
-                                echo " As you like :) "
-                            else
-                                echo "you enterd wrong input it must be Y/y or N/n Only";
-                                ((j--))
+    for ((i = 0; i < $len; i++)); do
+        if [[ $column == ${feilds[$i]} ]]; then
+            read -p "$column = " value
+            ((i++))
+            # echo "iteration : " $i;
+            valuesOfColumn=($(sed '1,2d' $name | cut -d' ' -f$i))
+            # echo ${valuesOfColumn[@]}
+            for ((j = 0; j < ${#valuesOfColumn[@]}; j++)); do
+                if [[ $value == ${valuesOfColumn[$j]} ]]; then
+                    let c=$j+3
+                    sed -n "$c"p $name
+                    read -p "Do you want to update this row ? Y/N" answer
+                    if [[ $answer == 'y' || $answer == 'Y' ]]; then
+                        read -p "enter new value : " newValue
+                        f=0
+                        for ((k = 0; k < ${#PKArray[@]}; k++)); do
+                            if [[ $newValue == ${PKArray[$k]} ]]; then
+                                f=1
+                                echo "Primary key must be unique !!!"
+                                break
                             fi
+                        done
+                        if [[ $f == 0 ]]; then
+                            sed -i "${c}s/$value/$newValue/" $name
                         fi
-                    done
+                    elif [[ $answer == 'n' || $answer == 'N' ]]; then
+                        echo " As you like :) "
+                    else
+                        echo "you enterd wrong input it must be Y/y or N/n Only"
+                        ((j--))
+                    fi
                 fi
             done
-else 
-echo "table not found"
+        fi
+    done
+else
+    echo "table not found"
 fi
-
 
 # update from tableName
 # set id = dgfsk
-# where id = 
+# where id =
